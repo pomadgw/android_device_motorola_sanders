dd2fb684d9afb30f6bcd3f694d4a56732d00e5bd
MasterAwesome <armu30@gmail.com>
2018-09-21 11:53:14 +0530

    sanders: fix wrong device name
    Signed-off-by: MasterAwesome <armu30@gmail.com>

--- arrow_sanders.mk
+++ arrow_sanders.mk
@@ -17,11 +17,12 @@
 # Inherit from those products. Most specific first.
 $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/product_launched_with_n_mr1.mk)
 
-# Inherit from potter device
+# Inherit from sanders device
 $(call inherit-product, device/motorola/sanders/device.mk)
 
-# Inherit some common CM stuff.
+# Inherit some common ArrowOS stuff.
 $(call inherit-product, vendor/arrow/config/common.mk)
 
 # Boot animation
