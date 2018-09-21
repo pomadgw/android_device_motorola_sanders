3f1d9b7d6c418c0ff7a275ecb96f86bbde524a66
MasterAwesome <armu30@gmail.com>
2018-09-21 11:49:57 +0530

    Build SnapdragonCamera
    Signed-off-by: MasterAwesome <armu30@gmail.com>

--- device.mk
+++ device.mk
@@ -99,7 +99,7 @@ PRODUCT_PACKAGES += \
 # Camera
 PRODUCT_PACKAGES += \
     libbson \
-    Camera2 \
+    SnapdragonCamera \
     libgui_vendor \
     camera.device@1.0-impl \
     camera.device@3.2-impl \
